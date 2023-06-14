3ce195fe40def2f2a89d85824b4680fbefb07e24
zerone015 <zerone015@naver.com>
2023-06-15 03:56:04 +0900

    feat: ft_strlen 테스트 코드 구현

---
+++ libasm.h
@@ -0,0 +1,8 @@
+#ifndef LIBASM_H_
+# define LIBASM_H_
+
+#include <stdio.h>
+
+size_t	ft_strlen(const char *str);
+
+#endif 
---
+++ main.c
@@ -0,0 +1,6 @@
+#include "libasm.h"
+
+int main(void) {
+	printf("ft_strlen = %zu \n", ft_strlen("yoson"));
+	return 0;	
+}
