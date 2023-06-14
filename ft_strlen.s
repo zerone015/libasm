dba3fb9ee894e382417674839226cd57ac472307
zerone015 <zerone015@naver.com>
2023-06-15 03:55:52 +0900

    feat: ft_strlen 구현

---
+++ ft_strlen.s
@@ -0,0 +1,15 @@
+section .text
+global _ft_strlen
+
+_ft_strlen:
+    mov rax, 0
+
+loop_start:
+    cmp byte [rdi + rax], 0
+    je loop_end
+
+    inc rax
+    jmp loop_start
+
+loop_end:
+    ret
