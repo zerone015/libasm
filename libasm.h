ae171eb421cc466f2916f0c3e5038bab9212824c
zerone015 <zerone015@naver.com>
2023-06-16 03:39:33 +0900

    feat: ft_read, ft_write 테스트 코드 추가

--- libasm.h
+++ libasm.h
@@ -1,10 +1,17 @@
 #ifndef LIBASM_H_
 # define LIBASM_H_
 
-#include <stdio.h>
+# define BUF_SIZE   20
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <fcntl.h>
 
 size_t	ft_strlen(const char *str);
 char    *ft_strcpy(char *dest, const char *src);
 int     ft_strcmp(const char* s1, const char* s2);
+ssize_t ft_write(int fd, const void *buf, size_t nbyte);
+ssize_t ft_read(int fd, void *buf, size_t nbyte);
 
 #endif 
--- main.c
+++ main.c
@@ -1,13 +1,57 @@
 #include "libasm.h"
 
 int main(void) {
-	char dest[] = "dest";
-	printf("ft_strlen = %zu \n", ft_strlen("yoson"));
-	printf("ft_strcpy = %s \n", ft_strcpy(dest, "src"));
-	printf("ft_strcpy = %s \n", ft_strcpy(dest, "crs"));
-	printf("ft_strcmp = %d \n", ft_strcmp("src", "src"));
-	printf("ft_strcmp = %d \n", ft_strcmp("srcA", "src"));
-	printf("ft_strcmp = %d \n", ft_strcmp("src", "srca"));
+	char *str = malloc(BUF_SIZE);
+	memset(str, 'a', BUF_SIZE);
+
+	printf("----------ft_strlen----------\n");
+	printf("expected: %d \n", BUF_SIZE);
+	printf("result: %zu \n", ft_strlen(str));
+	printf("expected: 0 \n");
+	printf("result: %zu \n", ft_strlen(""));
+
+	printf("----------ft_strcpy----------\n");
+	char *temp = malloc(BUF_SIZE);
+	printf("expected: %s \n", str);
+	printf("result: %s \n", ft_strcpy(temp, str));
+	printf("expected: %s \n", "");
+	printf("result: %s \n", ft_strcpy(temp, ""));
+
+	printf("----------ft_strcmp----------\n");
+	printf("expected: 0 \n");
+	printf("result: %d \n", ft_strcmp("", ""));
+	printf("expected: positive number \n");
+	printf("result: %d \n", ft_strcmp("src", ""));
+	printf("expected: negative number \n");
+	printf("result: %d \n", ft_strcmp("", "src"));
+	printf("expected: 0 \n");
+	printf("result: %d \n", ft_strcmp(str, str));
+	printf("expected: negative number \n");
+	printf("result: %d \n", ft_strcmp(str, "aaaaab"));
+	printf("expected: positive number \n");
+	printf("result: %d \n", ft_strcmp("aaaaab", str));
+
+	printf("----------ft_write----------\n");
+	printf("expected: \n%s \n", str);
+	printf("result: \n");
+	ssize_t write_size = ft_write(1, str, BUF_SIZE);
+	printf("\n");
+	printf("expected: %d \n", BUF_SIZE);
+	printf("result: %zd \n", write_size);
+	printf("expected: -1 \n");
+	printf("result: %zd \n", ft_write(-1, str, BUF_SIZE));
+	perror("perror");
+
+	printf("----------ft_read----------\n");
+	char *buf = calloc(1, BUF_SIZE);
+	printf("please input. \n");
+	printf("stdin test: %zd \n", ft_read(0, buf, BUF_SIZE));
+	int fd = open("ft_read.s", O_RDONLY);
+	printf("ft_read.s size: %zd \n", ft_read(fd, buf, BUF_SIZE));
+	printf("ft_read.s contents = %s \n", buf);
+	printf("expected: -1 \n");
+	printf("result = %zd \n", ft_read(-1, buf, BUF_SIZE));
+	perror("perror");
 
 	return 0;	
 }
