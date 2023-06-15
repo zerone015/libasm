1c0f7f4509258e2bf739a5c0e30b192a08efab4b
zerone015 <zerone015@naver.com>
2023-06-15 22:31:22 +0900

    feat: ft_strcpy 테스트 코드 구현

--- libasm.h
+++ libasm.h
@@ -4,5 +4,6 @@
 #include <stdio.h>
 
 size_t	ft_strlen(const char *str);
+char    *ft_strcpy(char *dest, const char *src);
 
 #endif 
--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include "libasm.h"
 
 int main(void) {
+	char dest[] = "dest";
 	printf("ft_strlen = %zu \n", ft_strlen("yoson"));
+	printf("ft_strcpy = %s \n", ft_strcpy(dest, "src"));
 	return 0;	
 }
