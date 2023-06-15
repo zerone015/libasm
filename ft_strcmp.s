e3ae55b38e053bc6e54dd30a00c04d5ddfd358a4
zerone015 <zerone015@naver.com>
2023-06-16 01:05:23 +0900

    feat: ft_strcmp 구현

---
+++ ft_strcmp.s
@@ -0,0 +1,21 @@
+section .text
+global _ft_strcmp
+
+_ft_strcmp:
+    mov rcx, 0
+
+loop_start:
+    cmp byte [rdi + rcx], 0
+    je loop_end
+    mov dl, [rdi + rcx]
+    cmp [rsi + rcx], dl
+    jne loop_end
+
+    inc rcx
+    jmp loop_start
+
+loop_end:
+    mov al, byte [rdi + rcx]
+    sub al, byte [rsi + rcx]
+    movsx eax, al
+    ret
--- main.c
+++ main.c
@@ -4,5 +4,10 @@ int main(void) {
 	char dest[] = "dest";
 	printf("ft_strlen = %zu \n", ft_strlen("yoson"));
 	printf("ft_strcpy = %s \n", ft_strcpy(dest, "src"));
+	printf("ft_strcpy = %s \n", ft_strcpy(dest, "crs"));
+	printf("ft_strcmp = %d \n", ft_strcmp("src", "src"));
+	printf("ft_strcmp = %d \n", ft_strcmp("srcA", "src"));
+	printf("ft_strcmp = %d \n", ft_strcmp("src", "srca"));
+
 	return 0;	
 }
