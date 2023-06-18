0264f5030b8f0a13b447f4f72cc2c5b4162a3ed2
zerone015 <zerone015@naver.com>
2023-06-19 03:48:05 +0900

    feat: ft_atoi_base 화이트 스페이스 스킵, 부호 스킵, 부호 설정 구현

--- ft_atoi_base.s
+++ ft_atoi_base.s
@@ -26,11 +26,38 @@ _base_duplication_check_loop:
     je _ft_atoi_base_err
     inc rdx
     jmp _base_duplication_check_loop
-
 _find_radix_end:
     cmp rcx, 2
     jl _ft_atoi_base_err
-
+    mov rdx, 0
+_skip_whitespace:
+    cmp rdi[rdx], 32
+    je _inc_skip_whitespace
+    cmp rdi[rdx], 9
+    jl _find_sign
+    cmp rdi[rdx], 13
+    jg _find_sign
+    jmp _inc_skip_whitespace
+_inc_skip_whitespace:
+    inc rdx
+    jmp _skip_whitespace
+_find_sign:
+    cmp rdi[rdx], 45
+    je _set_negative
+    cmp rdi[rdx], 43
+    je _set_positive
+    jmp _ft_atoi
+_set_negative:
+    mov rax, -1
+    inc rdx
+    jmp _find_sign
+_set_positive:
+    mov rax, 1
+    inc rdx
+    jmp _find_sign
+_ft_atoi:
+    ; 여기 ㄱㄱ
+    
 
 _ft_atoi_base_err:
     mov rax, 0
