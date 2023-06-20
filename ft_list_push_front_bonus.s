81a6903fffba202d52795aa5421ea90d2182d3b7
zerone015 <zerone015@naver.com>
2023-06-21 02:42:20 +0900

    chore: ft_list_push_front 주석 추가

--- ft_list_push_front_bonus.s
+++ ft_list_push_front_bonus.s
@@ -2,19 +2,19 @@ section .text
     extern _malloc
     global _ft_list_push_front
 _ft_list_push_front:
-    push rcx
-    push rdi
-    push rsi
-    mov rdi, 16
-    call _malloc
-    pop rsi
-    pop rdi
-    pop rcx
-    cmp rax, 0
-    je _end
-    mov [rax], rsi
-    mov rcx, [rdi]
-    mov [rax + 8], rcx
-    mov [rdi], rax
+    sub rsp, 24                 
+    mov [rsp + 16], rdi             ; 휘발성 레지스터에 저장된 전달인자 스택에 보존
+    mov [rsp + 8], rsi              ; 휘발성 레지스터에 저장된 전달인자 스택에 보존
+    mov rdi, 16                     ; rdi에 구조체 크기인 16 저장
+    call _malloc                    ; malloc 호출
+    mov rdi, [rsp + 16]             ; 스택에 보존되어있던 전달인자 복구
+    mov rsi, [rsp + 8]              ; 스택에 보존되어있던 전달인자 복구
+    cmp rax, 0                      ; malloc 반환 값 검사
+    je _end                         ; null 반환되었으면 그대로 종료
+    mov [rax], rsi                  ; 새로 생성된 원소의 data 저장
+    mov rcx, [rdi]                  ; 기존 첫 번째 원소를 메모리에서 레지스터로 적재
+    mov [rax + 8], rcx              ; 기존 첫 번째 원소를 새로 생성된 원소에 연결
+    mov [rdi], rax                  ; 새로 생성된 원소를 리스트의 첫 번째 원소로 설정
 _end:
+    add rsp, 24
     ret
